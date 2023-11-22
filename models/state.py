739b5dbce36865ec73866305b50af8eb5b8533d7
Nitsu47 <agustraversa2004@gmail.com>
2023-11-22 17:18:23 -0300

    state model

--- models/state.py
+++ models/state.py
@@ -1,8 +1,13 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy.orm import Column, Integer, String
 
-
+Base = declarative_base()
 class State(BaseModel):
-    """ State class """
-    name = ""
+    """ State Class """
+    __tablename__ = "state"
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", backref="state", cascade='all, delete, delete-orphan')
