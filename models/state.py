d5a6e69e202ab263cc8b4e91793e93ee2f85e895
FrancoMuVa <francomuva@gmail.com>
2023-11-22 22:28:41 +0000

    refactor: city, base model classes. fix: state style

--- models/state.py
+++ models/state.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy.orm import Column, Integer, String
 
-Base = declarative_base()
-class State(BaseModel):
+
+class State(BaseModel, Base):
     """ State Class """
     __tablename__ = "state"
     name = Column(String(128), nullable=False)
-    cities = relationship("City", backref="state", cascade='all, delete, delete-orphan')
+    cities = relationship("City", backref="state",
+                          cascade='all, delete, delete-orphan')
