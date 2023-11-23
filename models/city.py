8ee4878c5f40cdd1d60c9c06a3731019ea641146
Nitsu47 <agustraversa2004@gmail.com>
2023-11-23 14:44:33 -0300

    city and user models updated with relationship

--- models/city.py
+++ models/city.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
-from models.base_model import BaseModel, Base
+from models.base_model import BaseModel
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import Column, Integer, String
+from sqlalchemy.orm import Column, Integer, String, Base
 from sqlalchemy import ForeignKey
+from sqlalchemy.orm import relationship
 
 
 class City(BaseModel, Base):
@@ -11,3 +12,4 @@ class City(BaseModel, Base):
     __tablename__ = "cities"
     state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
     name = Column(String(128), nullable=False)
+    places = relationship("Place", backref="cities", cascade='all, delete, delete-orphan')
