ba7bc057de375d701c1a06be3cbd6febf129d86e
FrancoMuVa <muvafranco@gmail.com>
2023-12-16 04:22:04 +0000

    feat: function that display "n is a number" only if n is a number

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+"""
+    Script that starts a Flask web application.
+"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def Hello_hbnb():
+    """ return Hello HBNB! """
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """ return HBNB! """
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def show_value(text):
+    """ display “C ” followed by the value of the text """
+    return f"C {text.replace('_', ' ')}"
+
+
+@app.route("/python/<text>", strict_slashes=False)
+def show_text_value(text):
+    """ display “Python ”, followed by the value of the text """
+    return f"Python {text.replace('_', ' ')}"
+
+
+@app.route("/python", strict_slashes=False)
+def show_default_text():
+    """ display “Python ”, followed by the value of the text """
+    return f"Python is cool"
+
+
+@app.route("/number/<int:n>", strict_slashes=False)
+def show_n(n):
+    """ display “n is a number” only if n is an integer """
+    return f"{n} is a number"
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
