95e0c4d00a89c3dc23cd1bfef8f7254f4e5dfa6d
FrancoMuVa <muvafranco@gmail.com>
2023-12-16 10:48:19 +0000

    refactor: updated some part of engine.

--- models/state.py
+++ models/state.py
@@ -30,3 +30,16 @@ class State(BaseModel, Base):
                 if city.state_id == self.id:
                     new_list.append(city)
             return new_list
+
+    if getenv("HBNB_TYPE_STORAGE") != "db":
+        @property
+        def cities(self):
+            """ 
+                Getter attribute that returns the list of City instances
+            """
+            from models import storage
+            new_list = []
+            for city in storage.all(City).values():
+                if city.state_id == self.id:
+                    new_list.append(city)
+            return new_list
